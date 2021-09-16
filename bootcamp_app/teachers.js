c307328f436cfc2aec3340d980f585d9174c53d7
Sara Sabet <sara.ilbeigi@gmail.com>
2021-09-16 02:07:17 +0000

    update teacher/student.js with Parameterized Query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,18 +8,25 @@ const pool = new Pool({
 });
 
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-}).catch(err => console.error('query error', err.stack));
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+  `;
+
+const cohortName = process.argv[2] || 'JUL02';
+const values = [cohortName];
+  
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  });
+
+
 
